8b81163029fe04c627d21d18bd5d3845c3719816
Alex <trieu.alex1@gmail.com>
2024-03-12 13:34:50 -0700

    updated portfolio page

--- src/pages/WorkDetails/WorkDetails.js
+++ src/pages/WorkDetails/WorkDetails.js
@@ -4,6 +4,30 @@ import { useParams } from "react-router-dom";
 import projects from "../../data/projects.json";
 import "./WorkDetails.css";
 
+import logo1 from "../../assets/images/logo1.png";
+import logo2 from "../../assets/images/logo2.png";
+import logo3 from "../../assets/images/logo3.png";
+import logo4 from "../../assets/images/logo5.png";
+
+import project1Image from "../../assets/images/1.webp";
+import project2Image from "../../assets/images/2.webp";
+import project3Image from "../../assets/images/3.webp";
+import project4Image from "../../assets/images/4.webp";
+
+const imageMap = {
+  1: project1Image,
+  2: project2Image,
+  3: project3Image,
+  4: project4Image,
+};
+
+const logoMap = {
+  1: logo1,
+  2: logo2,
+  3: logo3,
+  4: logo4,
+};
+
 export default function WorkDetails() {
   let { projectId } = useParams();
   const project = projects.find((p) => p.id === parseInt(projectId));
@@ -17,20 +41,38 @@ export default function WorkDetails() {
       <div className="info-wrapper">
         <div class="info-container">
           <div class="info-item">
-            <span class="info-title">Client:</span>
+            <span class="info-title">Company:</span>
             <span class="info">{project.name}</span>
           </div>
           <div class="info-item">
             <span class="info-title">Project Type:</span>
-            <span class="info">info</span>
+            <span class="info">Design, Development, SEO/CRO</span>
+          </div>
+          <div class="info-item">
+            <span class="info-title">Tech Stack:</span>
+            <span class="info">{project.techStack}</span>
           </div>
           <div class="info-item">
             <span class="info-title">Year:</span>
             <span class="info">{project.year}</span>
           </div>
         </div>
+        <div className="project-overview-container">
+          <div className="project-overview">
+            <img
+              src={logoMap[project.image]}
+              className="project-overview-logo"
+            />
+            <p className="project-overview-description">
+              {project.projectDescription}
+            </p>
+          </div>
+          <a href={project.website} className="visit-website">
+            Visit Website →
+          </a>
+          <img className="project-overview" src={imageMap[project.image]} />
+        </div>
       </div>
-      <h1>Mei Garden</h1>
     </MainLayout>
   );
 }
